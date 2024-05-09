e4ffa276b4381671c40e09428a316b1e638030a8
chaimakechtaf <chaimakechtaf7@gmail.com>
2024-05-09 22:01:23 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:pfe_ecommerce/screens/home_screen.dart';
 import 'package:pfe_ecommerce/screens/splash_screen.dart';
 import 'package:pfe_ecommerce/services/auth.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
